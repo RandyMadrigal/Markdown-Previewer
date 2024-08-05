a188779298793adc28e2935229364c847e3b57cd
Randy Madrigal <randymadrigalx12@gmail.com>
2024-08-04 23:25:37 -0400

    add img

--- src/components/markdown/MarkDownContainer.jsx
+++ src/components/markdown/MarkDownContainer.jsx
@@ -28,7 +28,7 @@ Inline code example: \`const x = 42;\`
       </head>
     </html>
 
-![React-logo](https://imgs.search.brave.com/PBObDQhElLLghbtGfzIbbeZ_G5wGbf2rCuVZ7CMUum4/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/aGFzaG5vZGUuY29t/L3Jlcy9oYXNobm9k/ZS9pbWFnZS91cGxv/YWQvdjE2NDc0OTA2/MTk5NjUvUDFkc05n/ai1mMS5wbmc)
+![React-logo](/assets/images/react-logo.png)
 
 `;
 
